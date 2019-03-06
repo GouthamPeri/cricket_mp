38ae935cb9bfbeada8780d7cff128906722ef453
GP250034 <goutham.peri@teradata.com>
2019-03-06 15:05:01 +0530

    updated python files

--- client.py
+++ client.py
@@ -2,18 +2,57 @@ import socket
 import sys
 import time
 
-conn = socket.socket()
-host = input(str("IP:"))
-port = 8080
 
-conn.connect((host, port))
+def send_client_name_and_receive_server_name(conn, CLIENT_NAME):
+        
+    # Encoding and sending the client name
+    client_name = CLIENT_NAME.encode()
+    conn.send(client_name)
 
-while True:
-    message = input(str(">>"))
-    message = message.encode()
+    # Receiving and initializing the clients name
 
-    conn.send(message)
+    server_name = conn.recv(1024)
+    server_name = server_name.decode()
+    return server_name
 
-    incoming_message = conn.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Server :" + incoming_message)
+
+def start_client():
+    SERVER_NAME = ''
+    CLIENT_NAME = ''
+
+    conn = socket.socket()
+    host = input(str("IP:"))
+    port = 8080
+
+    conn.connect((host, port))
+
+    print("Please Enter your name:", end = '')
+    CLIENT_NAME = input().strip()
+
+    if not CLIENT_NAME:
+        CLIENT_NAME = 'Player 2'
+
+    SERVER_NAME = send_client_name_and_receive_server_name(conn, CLIENT_NAME)
+
+    while True:
+        outgoing_message = input(str(">>"))
+        outgoing_message = outgoing_message[0]
+
+
+        # Error Handling
+        if (ord(outgoing_message[0]) < ord('0')
+            or ord(outgoing_message[0]) > ord('6')):
+            outgoing_message = '6'
+
+        # Sending the outgoing_message
+        encoded_outgoing_message = outgoing_message.encode()
+        conn.send(encoded_outgoing_message)
+
+        # Receiving the incoming_message
+        incoming_message = conn.recv(1024)
+        incoming_message = incoming_message.decode()
+        print(str(SERVER_NAME) + " :" + incoming_message)
+        print(str(CLIENT_NAME) + " :" + outgoing_message)
+
+
+start_client()
--- server.py
+++ server.py
@@ -2,24 +2,59 @@ import socket
 import sys
 import time
 
-s = socket.socket()
-host = socket.gethostname()
-ipaddr = socket.gethostbyname(host)
-print("Server will start on :" + ipaddr)
-port = 8080
 
-s.bind((host, port))
+def send_server_name_and_receive_client_name(conn, SERVER_NAME):
+    
+    # Encoding and sending the server name
+    server_name = SERVER_NAME.encode()
+    conn.send(server_name)
 
-s.listen(1)
+    # Receiving and initializing the clients name
 
-conn, addr = s.accept()
+    client_name = conn.recv(1024)
+    client_name = client_name.decode()
+    return client_name
 
-while True:
-    message = input(str(">>"))
-    message = message.encode()
 
-    conn.send(message)
+def start_server():
+    SERVER_NAME = ''
+    CLIENT_NAME = ''
 
-    incoming_message = conn.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Client :" + incoming_message)
+    s = socket.socket()
+    host = socket.gethostname()
+    ipaddr = socket.gethostbyname(host)
+    print("Server will start on :" + ipaddr)
+    port = 8080
+    s.bind((host, port))
+    s.listen(1)
+    conn, addr = s.accept()
+
+    print("Please Enter your name:", end = '')
+    SERVER_NAME = input().strip()
+
+    if not SERVER_NAME:
+        SERVER_NAME = 'Player 1'
+
+    CLIENT_NAME = send_server_name_and_receive_client_name(conn, SERVER_NAME)
+
+    while True:
+        outgoing_message = input(str(">>"))
+        outgoing_message = outgoing_message[0]
+
+
+        # Error Handling
+        if (ord(outgoing_message[0]) < ord('0')
+            or ord(outgoing_message[0]) > ord('6')):
+            outgoing_message = '6'
+
+        # Sending the outgoing_message
+        encoded_outgoing_message = outgoing_message.encode()
+        conn.send(encoded_outgoing_message)
+
+        # Receiving the incoming_message
+        incoming_message = conn.recv(1024)
+        incoming_message = incoming_message.decode()
+        print(SERVER_NAME + " :" + outgoing_message)
+        print(CLIENT_NAME + " :" + incoming_message)
+
+start_server()
